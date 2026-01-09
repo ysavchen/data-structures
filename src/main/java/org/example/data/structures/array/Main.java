61d8fff7d8e9c640d977c32e915e17026cbc9ede
Yuriy Savchenko <ysavchen@outlook.com>
2026-01-09 13:58:19 +0300

    Update tests

--- src/main/java/org/example/data/structures/array/Main.java
+++ src/main/java/org/example/data/structures/array/Main.java
@@ -3,7 +3,7 @@ package org.example.data.structures.array;
 public class Main {
 
     public static void main(String[] args) {
-        var array = new SortedArray<String>(3);
+        var array = new SortedArrayImpl<String>(3);
         array.addElement("Maria");
         array.addElement("Elena");
         array.addElement("Anna");
