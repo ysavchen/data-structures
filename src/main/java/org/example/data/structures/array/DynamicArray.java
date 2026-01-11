8e50e6c7cf1d04438fcfe2b8c0d40ee4474bbb95
Yuriy Savchenko <ysavchen@outlook.com>
2026-01-11 21:52:27 +0300

    Update DynamicArray

--- src/main/java/org/example/data/structures/array/DynamicArray.java
+++ src/main/java/org/example/data/structures/array/DynamicArray.java
@@ -5,6 +5,18 @@ package org.example.data.structures.array;
  */
 public class DynamicArray<T> {
 
+    private final T[] elementData;
+
+    private int initialCapacity = 10;
+
     public DynamicArray() {
+        this.elementData = (T[]) new Comparable[initialCapacity];
+    }
+
+    public DynamicArray(int initialCapacity) {
+        if (initialCapacity <= 0) {
+            throw new IllegalArgumentException("Initial capacity must be greater than 0");
+        }
+        this.elementData = (T[]) new Comparable[initialCapacity];
     }
 }
