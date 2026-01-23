ba85751925314a64a9215a831a7644a3be200c6f
Yuriy Savchenko <ysavchen@outlook.com>
2026-01-23 18:41:05 +0300

    Rough sorted list

--- src/main/java/org/example/data/structures/linkedlist/SortedSinglyLinkedList.java
+++ src/main/java/org/example/data/structures/linkedlist/SortedSinglyLinkedList.java
@@ -1,7 +1,46 @@
 package org.example.data.structures.linkedlist;
 
 /**
- * Отсортированный односвязный список
+ * Отсортированный односвязный список<br/>
+ * Для операций со связными списками все равно нужно перебирать элементы, поэтому сортировка не приносит особой пользы.
  */
-public class SortedSinglyLinkedList<T extends Comparable<T>> {
+public class SortedSinglyLinkedList<T extends Comparable<T>> implements SortedLinkedList<T> {
+
+    private Node<T> head = null;
+
+    public SortedSinglyLinkedList() {
+    }
+
+    @Override
+    public void add(T element) {
+
+    }
+
+    @Override
+    public int getPosition(T element) {
+        return 0;
+    }
+
+    @Override
+    public void delete(T element) {
+
+    }
+
+    private static class Node<T> {
+
+        private final T data;
+        private Node<T> nextNode = null;
+
+        Node(T data) {
+            this.data = data;
+        }
+
+        boolean hasNextNode() {
+            return nextNode != null;
+        }
+
+        void appendNode(Node<T> node) {
+            nextNode = node;
+        }
+    }
 }
