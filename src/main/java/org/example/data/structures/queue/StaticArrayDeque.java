823c95881b9f34d4ca8e854d2f468462fa3883ec
Yuriy Savchenko <ysavchen@outlook.com>
2026-03-05 11:54:54 +0300

    Rough StaticArrayDeque

---
+++ src/main/java/org/example/data/structures/queue/StaticArrayDeque.java
@@ -0,0 +1,8 @@
+package org.example.data.structures.queue;
+
+/**
+ * Deque - double ended queue (двусторонняя очередь)<br/>
+ * Дек позволяет добавлять и извлекать элементы с обоих концов.
+ */
+public class StaticArrayDeque<T> {
+}
