706087d13028edb61fd74930af884939bfaf9918
Yuriy Savchenko <ysavchen@outlook.com>
2026-03-06 13:54:16 +0300

    Update Countdown

--- src/main/java/org/example/algorithms/recursion/Countdown.java
+++ src/main/java/org/example/algorithms/recursion/Countdown.java
@@ -9,7 +9,7 @@ public class Countdown {
      * В базовом случае функция себя не вызывает... чтобы предотвратить зацикливание.
      */
     private static void countdown(int i) {
-        System.out.println("i = " + i);
+        System.out.println(i);
 
         if (i <= 0) {
             return; // базовый случай
