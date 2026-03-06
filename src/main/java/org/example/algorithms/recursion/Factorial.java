c2ee2c72f33481415c16922f1ef6254942e05847
Yuriy Savchenko <ysavchen@outlook.com>
2026-03-06 13:50:44 +0300

    Update Factorial

--- src/main/java/org/example/algorithms/recursion/Factorial.java
+++ src/main/java/org/example/algorithms/recursion/Factorial.java
@@ -2,16 +2,31 @@ package org.example.algorithms.recursion;
 
 public class Factorial {
 
-    public static void main(String[] args) {
-        int result = fact(3);
-        System.out.println("Result: " + result);
+    /**
+     * Вычисление факториала с помощью цикла
+     */
+    public static int loopFactorial(int x) {
+        int accumulator = 1;
+        int i = x;
+        while (i > 1) {
+            accumulator *= i;
+            i -= 1;
+        }
+        return accumulator;
     }
 
-    private static int fact(int x) {
-        if (x == 1) {
+    /**
+     * Вычисление факториала с помощью рекурсии
+     */
+    private static int factorial(int x) {
+        if (x == 1 || x == 0) {
             return 1;
-        } else {
-            return x * fact(x - 1);
         }
+        return x * factorial(x - 1);
+    }
+
+    public static void main(String[] args) {
+        int result = factorial(3);
+        System.out.println("Result: " + result);
     }
 }
