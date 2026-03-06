59dcf15008de21c633356e82a057771de45db404
Yuriy Savchenko <ysavchen@outlook.com>
2026-03-06 13:53:10 +0300

    Update Countdown

--- src/main/java/org/example/algorithms/recursion/Countdown.java
+++ src/main/java/org/example/algorithms/recursion/Countdown.java
@@ -2,17 +2,13 @@ package org.example.algorithms.recursion;
 
 public class Countdown {
 
-    public static void main(String[] args) {
-        new Countdown().countdown(10);
-    }
-
     /**
      * Когда вы пишете рекурсивную функцию, в ней необходимо указать, в какой момент следует прервать рекурсию.
      * Вот почему каждая рекурсивная функция состоит из двух частей: базового случая и рекурсивного случая.
      * В рекурсивном случае функция вызывает сама себя.
      * В базовом случае функция себя не вызывает... чтобы предотвратить зацикливание.
      */
-    private void countdown(int i) {
+    private static void countdown(int i) {
         System.out.println("print " + i);
 
         if (i <= 0) {
@@ -20,5 +16,9 @@ public class Countdown {
         } else {
             countdown(i - 1); // рекурсивый случай
         }
+    }
+
+    public static void main(String[] args) {
+        countdown(10);
     }
 }
