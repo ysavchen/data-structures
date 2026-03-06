9541d180594dd0b04a5330db73a8db30f91b579f
Yuriy Savchenko <ysavchen@outlook.com>
2026-03-07 00:35:38 +0300

    Update LinkedListStack

--- src/main/java/org/example/data/structures/stack/LinkedListStack.java
+++ src/main/java/org/example/data/structures/stack/LinkedListStack.java
@@ -1,7 +1,45 @@
 package org.example.data.structures.stack;
 
+import org.example.data.structures.linkedlist.SinglyLinkedList;
+
 /**
  * Стек на основе односвязного списка
  */
-public class LinkedListStack {
+public class LinkedListStack<T> implements Stack<T> {
+
+    private final SinglyLinkedList<T> list;
+
+    public LinkedListStack() {
+        list = new SinglyLinkedList<>();
+    }
+
+    @Override
+    public void push(T item) {
+        list.insertToFront(item);
+    }
+
+    @Override
+    public T pop() {
+        if (isEmpty()) {
+            return null;
+        }
+        T element = list.getFirstElement();
+        list.delete(element);
+        return element;
+    }
+
+    @Override
+    public T peek() {
+        return isEmpty() ? null : list.getFirstElement();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    @Override
+    public int getSize() {
+        return list.getSize();
+    }
 }
