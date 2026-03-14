530f96961472e8269b5cc4a577fdfff18c3edb37
Yuriy Savchenko <ysavchen@outlook.com>
2026-03-14 03:05:30 +0300

    Update queue

--- src/main/java/org/example/data/structures/queue/LinkedListQueue.java
+++ src/main/java/org/example/data/structures/queue/LinkedListQueue.java
@@ -1,7 +1,36 @@
 package org.example.data.structures.queue;
 
+import org.example.data.structures.linkedlist.DoublyLinkedList;
+
 /**
  * Очередь на основе двусвязного списка
  */
-public class LinkedListQueue<T> {
+public class LinkedListQueue<T> implements Queue<T> {
+
+    private final DoublyLinkedList<T> list;
+
+    public LinkedListQueue() {
+        this.list = new DoublyLinkedList<>();
+    }
+
+    @Override
+    public boolean enqueue(T element) {
+        list.insertToBack(element);
+        return true;
+    }
+
+    @Override
+    public T dequeue() {
+        return list.getFirstElement();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    @Override
+    public int getSize() {
+        return list.getSize();
+    }
 }
