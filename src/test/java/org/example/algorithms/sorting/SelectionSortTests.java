0594b9d27cee6960968012a10efd585c1a693f24
Yuriy Savchenko <ysavchen@outlook.com>
2026-03-23 12:41:01 +0300

    Update test

--- src/test/java/org/example/algorithms/sorting/SelectionSortTests.java
+++ src/test/java/org/example/algorithms/sorting/SelectionSortTests.java
@@ -2,37 +2,39 @@ package org.example.algorithms.sorting;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class SelectionSortTests {
 
     @Test
-    void selectionSort() {
-        var immutable = List.of(9, 14, 2, 5);
-        var actual = SelectionSort.sort(immutable);
-        var expected = new ArrayList<>(immutable);
-        Collections.sort(expected);
-        assertEquals(expected, actual);
+    void sort() {
+        Integer[] array = {9, 14, 2, 5};
+        Integer[] sortedArray = {2, 5, 9, 14};
+        SelectionSort.sort(array);
+        assertArrayEquals(sortedArray, array);
     }
 
     @Test
-    void selectionSortSameElements() {
-        var immutable = List.of(1, 1, 1);
-        var actual = SelectionSort.sort(immutable);
-        var expected = new ArrayList<>(immutable);
-        Collections.sort(expected);
-        assertEquals(expected, actual);
+    void sortWithSameElements() {
+        Integer[] array = {9, 14, 2, 2, 5};
+        Integer[] sortedArray = {2, 2, 5, 9, 14};
+        SelectionSort.sort(array);
+        assertArrayEquals(sortedArray, array);
     }
 
     @Test
-    void sortEmptyList() {
-        List<Integer> immutable = Collections.emptyList();
-        List<Integer> sorted = SelectionSort.sort(immutable);
-        assertTrue(sorted.isEmpty());
+    void sortSortedArray() {
+        Integer[] array = {2, 5, 9, 14};
+        Integer[] sortedArray = {2, 5, 9, 14};
+        SelectionSort.sort(array);
+        assertArrayEquals(sortedArray, array);
+    }
+
+    @Test
+    void sortEmptyArray() {
+        Integer[] array = {};
+        Integer[] sortedArray = {};
+        SelectionSort.sort(array);
+        assertArrayEquals(sortedArray, array);
     }
 }
