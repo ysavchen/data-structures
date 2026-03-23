093dfd22fb8f20533e7d09e0e0fd5e70c313f671
Yuriy Savchenko <ysavchen@outlook.com>
2026-03-23 15:12:20 +0300

    Add tests

---
+++ src/test/java/org/example/algorithms/sorting/InsertionSortTests.java
@@ -0,0 +1,40 @@
+package org.example.algorithms.sorting;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class InsertionSortTests {
+
+    @Test
+    void sort() {
+        Integer[] array = {9, 14, 2, 5};
+        Integer[] sortedArray = {2, 5, 9, 14};
+        InsertionSort.sort(array);
+        assertArrayEquals(sortedArray, array);
+    }
+
+    @Test
+    void sortWithSameElements() {
+        Integer[] array = {9, 14, 2, 2, 5};
+        Integer[] sortedArray = {2, 2, 5, 9, 14};
+        InsertionSort.sort(array);
+        assertArrayEquals(sortedArray, array);
+    }
+
+    @Test
+    void sortSortedArray() {
+        Integer[] array = {2, 5, 9, 14};
+        Integer[] sortedArray = {2, 5, 9, 14};
+        InsertionSort.sort(array);
+        assertArrayEquals(sortedArray, array);
+    }
+
+    @Test
+    void sortEmptyArray() {
+        Integer[] array = {};
+        Integer[] sortedArray = {};
+        InsertionSort.sort(array);
+        assertArrayEquals(sortedArray, array);
+    }
+}
