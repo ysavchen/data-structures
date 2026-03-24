8d1c1ee438445314ebc5154de8e73cee2353a742
Yuriy Savchenko <ysavchen@outlook.com>
2026-03-24 16:58:02 +0300

    Update Fibonacci

--- src/main/java/org/example/algorithms/recursion/Fibonacci.java
+++ src/main/java/org/example/algorithms/recursion/Fibonacci.java
@@ -1,4 +1,16 @@
 package org.example.algorithms.recursion;
 
 public class Fibonacci {
+
+    private static int fibonacci(int x) {
+        if (x == 1 || x == 0) {
+            return 1;
+        }
+        return fibonacci(x - 1) + fibonacci(x - 2);
+    }
+
+    public static void main(String[] args) {
+        int result = fibonacci(3);
+        System.out.println("Result: " + result);
+    }
 }
