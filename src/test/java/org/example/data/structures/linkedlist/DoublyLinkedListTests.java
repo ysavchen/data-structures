ca6c0cf4e8ac76bd1620041a757e32b0d57e41d7
Yuriy Savchenko <ysavchen@outlook.com>
2026-03-30 11:02:55 +0300

    Update tests

--- src/test/java/org/example/data/structures/linkedlist/DoublyLinkedListTests.java
+++ src/test/java/org/example/data/structures/linkedlist/DoublyLinkedListTests.java
@@ -2,7 +2,7 @@ package org.example.data.structures.linkedlist;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class DoublyLinkedListTests {
 
@@ -42,5 +42,62 @@ public class DoublyLinkedListTests {
         assertEquals(2, list.getSize());
         assertEquals(anna, list.getFirstElement());
         assertEquals(elena, list.getLastElement());
+    }
+
+    @Test
+    void deleteFirstElement() {
+        var list = new DoublyLinkedList<String>();
+        var anna = "Anna";
+        var elena = "Elena";
+        list.insertToBack(anna);
+        list.insertToBack(elena);
+
+        var result = list.delete(anna);
+        assertTrue(result);
+        assertEquals(1, list.getSize());
+        assertEquals(elena, list.getFirstElement());
+        assertEquals(elena, list.getLastElement());
+    }
+
+    @Test
+    void deleteMiddleElement() {
+        var list = new DoublyLinkedList<String>();
+        var anna = "Anna";
+        var elena = "Elena";
+        var maria = "Maria";
+        list.insertToBack(anna);
+        list.insertToBack(elena);
+        list.insertToBack(maria);
+
+        var result = list.delete(elena);
+        assertTrue(result);
+        assertEquals(2, list.getSize());
+        assertEquals(anna, list.getFirstElement());
+        assertEquals(maria, list.getLastElement());
+    }
+
+    @Test
+    void deleteLastElement() {
+        var list = new DoublyLinkedList<String>();
+        var anna = "Anna";
+        var elena = "Elena";
+        list.insertToBack(anna);
+        list.insertToBack(elena);
+
+        var result = list.delete(elena);
+        assertTrue(result);
+        assertEquals(1, list.getSize());
+        assertEquals(anna, list.getLastElement());
+    }
+
+    @Test
+    void deleteNonExistingElement() {
+        var list = new DoublyLinkedList<String>();
+        var anna = "Anna";
+        list.insertToBack(anna);
+
+        var result = list.delete("Elena");
+        assertFalse(result);
+        assertEquals(1, list.getSize());
     }
 }
