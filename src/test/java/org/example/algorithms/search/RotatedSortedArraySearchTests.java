8e6c7ac5dba306b11fee65a1c9fa9249eaa23a44
Yuriy Savchenko <ysavchen@outlook.com>
2026-03-31 16:29:53 +0300

    Add tests

---
+++ src/test/java/org/example/algorithms/search/RotatedSortedArraySearchTests.java
@@ -0,0 +1,64 @@
+package org.example.algorithms.search;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RotatedSortedArraySearchTests {
+
+    @Test
+    void searchSortedArrayFirstPart() {
+        int[] array = {1, 2, 3, 4, 5, 6, 7};
+        int target = 3;
+        int index = RotatedSortedArraySearch.search(array, target);
+        assertEquals(2, index);
+    }
+
+    @Test
+    void searchSortedArraySecondPart() {
+        int[] array = {1, 2, 3, 4, 5, 6, 7};
+        int target = 5;
+        int index = RotatedSortedArraySearch.search(array, target);
+        assertEquals(4, index);
+    }
+
+    @Test
+    void searchMiddleElement() {
+        int[] array = {5, 6, 7, 1, 2, 3, 4};
+        int target = 1;
+        int index = RotatedSortedArraySearch.search(array, target);
+        assertEquals(3, index);
+    }
+
+    @Test
+    void searchSortedFirstPart() {
+        int[] array = {5, 6, 7, 1, 2, 3, 4};
+        int target = 7;
+        int index = RotatedSortedArraySearch.search(array, target);
+        assertEquals(2, index);
+    }
+
+    @Test
+    void searchSortedSecondPart() {
+        int[] array = {5, 6, 7, 1, 2, 3, 4};
+        int target = 2;
+        int index = RotatedSortedArraySearch.search(array, target);
+        assertEquals(4, index);
+    }
+
+    @Test
+    void searchLastElement() {
+        int[] array = {2, 3, 4, 5, 6, 7, 1};
+        int target = 1;
+        int index = RotatedSortedArraySearch.search(array, target);
+        assertEquals(6, index);
+    }
+
+    @Test
+    void searchUnsortedPart() {
+        int[] array = {7, 4, 6, 5, 1, 2, 3};
+        int target = 4;
+        int index = RotatedSortedArraySearch.search(array, target);
+        assertEquals(-1, index);
+    }
+}
