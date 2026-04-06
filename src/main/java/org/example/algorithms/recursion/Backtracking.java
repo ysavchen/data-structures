9b0c74afee12c13e6032e61a14ed25587bff8a89
Yuriy Savchenko <ysavchen@outlook.com>
2026-04-06 14:35:30 +0300

    Update backtracking

--- src/main/java/org/example/algorithms/recursion/Backtracking.java
+++ src/main/java/org/example/algorithms/recursion/Backtracking.java
@@ -16,14 +16,14 @@ public class Backtracking {
         }
     }
 
-    private static void generate2(String current, int n) {
+    private static void generate2(String buffer, int n) {
         if (n == 0) {
-            System.out.println(current);
+            System.out.println(buffer);
             return;
         }
 
-        generate2(current + "a", n - 1);
-        generate2(current + "b", n - 1);
+        generate2(buffer + "a", n - 1);
+        generate2(buffer + "b", n - 1);
     }
 
     public static void main(String[] args) {
