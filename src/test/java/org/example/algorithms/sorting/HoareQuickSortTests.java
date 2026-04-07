363a344d0a3fea43c171b336e2f5abf12b1b5523
Yuriy Savchenko <ysavchen@outlook.com>
2026-04-08 00:18:12 +0300

    Add tests

---
+++ src/test/java/org/example/algorithms/sorting/HoareQuickSortTests.java
@@ -0,0 +1,48 @@
+package org.example.algorithms.sorting;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class HoareQuickSortTests {
+
+    @Test
+    void sort() {
+        Integer[] array = {9, 14, 2, 5};
+        Integer[] sortedArray = {2, 5, 9, 14};
+        HoareQuickSort.sort(array, 0, array.length - 1);
+        assertArrayEquals(sortedArray, array);
+    }
+
+    @Test
+    void sortWithSameElements() {
+        Integer[] array = {9, 14, 2, 2, 5};
+        Integer[] sortedArray = {2, 2, 5, 9, 14};
+        HoareQuickSort.sort(array, 0, array.length - 1);
+        assertArrayEquals(sortedArray, array);
+    }
+
+    @Test
+    void sortNegativeElements() {
+        Integer[] array = {-5, 4, 1, -3, 0};
+        Integer[] sortedArray = {-5, -3, 0, 1, 4};
+        HoareQuickSort.sort(array, 0, array.length - 1);
+        assertArrayEquals(sortedArray, array);
+    }
+
+    @Test
+    void sortSortedArray() {
+        Integer[] array = {2, 5, 9, 14};
+        Integer[] sortedArray = {2, 5, 9, 14};
+        HoareQuickSort.sort(array, 0, array.length - 1);
+        assertArrayEquals(sortedArray, array);
+    }
+
+    @Test
+    void sortEmptyArray() {
+        Integer[] array = {};
+        Integer[] sortedArray = {};
+        HoareQuickSort.sort(array, 0, array.length - 1);
+        assertArrayEquals(sortedArray, array);
+    }
+}
