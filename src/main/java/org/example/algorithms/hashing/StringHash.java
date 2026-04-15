906eb5a4d82189bb6ef0ef9cd3a5341bf932e6b3
Yuriy Savchenko <ysavchen@outlook.com>
2026-04-15 15:36:18 +0300

    Update StringHash

--- src/main/java/org/example/algorithms/hashing/StringHash.java
+++ src/main/java/org/example/algorithms/hashing/StringHash.java
@@ -4,4 +4,27 @@ package org.example.algorithms.hashing;
  * Хеш строки
  */
 public class StringHash {
+
+    /**
+     * Полиномиальный хеш считается по формуле:<br/>
+     *
+     * <p>
+     * Хорошие простые числа:<br/>
+     * {@code 10^9 + 7}<br/>
+     * {@code 10^9 + 9}
+     * <p>
+     * Время работы: O(n)
+     */
+    public static long hash(String str) {
+        long p = 1_000_000_007L; // 10^9 + 7
+        long r = 4_294_967_296L; // 2^32
+
+        long h = 0L;
+        char[] chars = str.toCharArray();
+        for (int i = 0; i < str.length(); i++) {
+            h = (h * p + chars[i]) % r;
+        }
+
+        return h;
+    }
 }
